cde2dc4d788ab38189fd8815fe61bb67bfbbb0fb
Denis <den.zhernov@yandex.ru>
2026-04-04 18:28:52 +0300

    добавил модель Booking

---
+++ EvoEvent.Web/EvoEvent.Web/Models/Booking/Booking.cs
@@ -0,0 +1,16 @@
+﻿namespace EvoEvent.Web.Models
+{
+	public class Booking
+	{
+		public Guid Id { get; init; } = Guid.NewGuid();
+		
+		public Guid EventId { get; init; }
+
+		public BookingStatus Status { get; set; } = BookingStatus.Pending;
+
+		public DateTime CreateAt { get; init; } = DateTime.Now;
+
+		public DateTime ProcessedAt { get; set; }
+	}
+
+}
