421db87dc7670a0ef03ed412086ae3bed3a1fbcf
Denis <den.zhernov@yandex.ru>
2026-05-02 11:41:48 +0300

    добавил конфигурации для моделей

---
+++ EvoEvent.Web/EvoEvent.Web/DataAccess/Configurations/BookingConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using EvoEvent.Web.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EvoEvent.Web.DataAccess.Configurations
+{
+	public class BookingConfiguration : IEntityTypeConfiguration<Booking>
+	{
+		public void Configure(EntityTypeBuilder<Booking> builder)
+		{
+
+			builder.ToTable("bookings", "catalog");
+
+			builder.HasKey(b => b.Id);
+			builder.Property(b => b.Id)
+				.ValueGeneratedNever();
+
+			builder.Property(b => b.Status)
+				.HasConversion<string>();
+
+
+			builder.HasOne(b => b.Event)
+				.WithMany(e => e.Bookings)
+				.HasForeignKey(b => b.EventId);
+		}
+	}
+}
