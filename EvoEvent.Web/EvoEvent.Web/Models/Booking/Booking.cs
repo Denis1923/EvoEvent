47dcd074f2c3295d58c293f56c76705037df1d1d
Denis <den.zhernov@yandex.ru>
2026-05-02 11:23:28 +0300

    Настроил связи

--- EvoEvent.Web/EvoEvent.Web/Models/Booking/Booking.cs
+++ EvoEvent.Web/EvoEvent.Web/Models/Booking/Booking.cs
@@ -4,7 +4,6 @@
 	{
 		public Guid Id { get; init; }
 		
-		public Guid EventId { get; init; }
 
 		public BookingStatus Status { get; set; }
 
@@ -12,12 +11,23 @@
 
 		public DateTime? ProcessedAt { get; set; }
 
+		public Guid EventId { get; init; }
+
+		public Event Event { get; set; }
+
 		public Booking(Guid? id, Guid eventId, BookingStatus status, DateTime сreatedAt)
 		{
 			Id = id ?? Guid.NewGuid();
 			EventId = eventId;
 			Status = status;
 			CreatedAt = сreatedAt;
+		}
+
+		// EF Core использует рефлексию для создания экземпляров сущностей при чтении данных из БД.
+		// Для этого ему необходим приватный конструктор без параметров.
+		private Booking() 
+		{
+
 		}
 	}
 
