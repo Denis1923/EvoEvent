c625ea7f53940fb390ff152ae6de320985ac9517
Denis <den.zhernov@yandex.ru>
2026-07-05 15:30:51 +0300

    Добавлена константа и модель контракта события

---
+++ EvoEvent.Web/CommonLibrary/Consts/ConstKafka.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonLibrary.Consts
+{
+	public static class ConstKafka
+	{
+		public const string bookingConf = "booking-confirmed";
+	}
+}
---
+++ EvoEvent.Web/CommonLibrary/Models/Kafka/BookingConfirmed.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonLibrary.Models.Kafka
+{
+	public class BookingConfirmed
+	{
+		public Guid BookingId { get; set; }
+
+		public Guid EventId { get; set; }
+
+		public Guid UserId { get; set; }
+
+		public int TotalSeats { get; set; }
+
+		public DateTime CreateAt { get; set; }
+	}
+}
